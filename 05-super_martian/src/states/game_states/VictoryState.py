07536b6d56d3a3c16dcabff972d5ba0495232f3a
fgrim4227 <grimaldofranciscojavier@gmail.com>
2026-09-03 16:40:46 -0400

    Erased renmant comment

--- 05-super_martian/src/states/game_states/VictoryState.py
+++ 05-super_martian/src/states/game_states/VictoryState.py
@@ -1,12 +1,3 @@
-"""
-ISPPV1 2023
-Study Case: Super Martian (Platformer)
-
-Author: Alejandro Mujica
-alejandro.j.mujic4@gmail.com
-
-This file contains the class PlayState.
-"""
 
 from typing import Dict, Any
 
